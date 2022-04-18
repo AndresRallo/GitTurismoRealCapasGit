d0191024a0696af1aca6cb84967b1102e8657d57
AndresRallo <48107738+AndresRallo@users.noreply.github.com>
2022-04-18 17:02:25 -0400

    capa de negocios clase creada para login

---
+++ CapaNegocio/CNLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+using System.Data;
+using System.Windows.Forms;
+
+namespace CapaNegocio
+{
+    public class CNLogin
+    {
+        public DataTable N_user(CEEmpleado obje)
+        {
+            DataTable dt = new DataTable();
+            CDLogin objd = new CDLogin();
+
+            return objd.D_user(obje);
+        }
+    }
+}
