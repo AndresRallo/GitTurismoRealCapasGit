6819d827263ee02ed912b9a6d31935c2d2282ff0
Beiconn69 <lorenpapo@gmail.com>
2022-05-28 01:13:37 -0400

    avance reservas

---
+++ CapaPresentacion/AgregarReserva.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class AgregarReserva
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblAgregarReserva = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(20, 55);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(316, 13);
+            this.lblDesc.TabIndex = 5;
+            this.lblDesc.Text = "* Para agregar reserva ingrese sus credenciales correspondientes";
+            // 
+            // lblAgregarReserva
+            // 
+            this.lblAgregarReserva.AutoSize = true;
+            this.lblAgregarReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblAgregarReserva.Location = new System.Drawing.Point(12, 9);
+            this.lblAgregarReserva.Name = "lblAgregarReserva";
+            this.lblAgregarReserva.Size = new System.Drawing.Size(319, 46);
+            this.lblAgregarReserva.TabIndex = 4;
+            this.lblAgregarReserva.Text = "Agregar Reserva";
+            // 
+            // AgregarReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblAgregarReserva);
+            this.Name = "AgregarReserva";
+            this.Text = "AgregarReserva";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblAgregarReserva;
+    }
+}
---
+++ CapaPresentacion/MantenedorReservas.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class MantenedorReservas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProbarConexion = new System.Windows.Forms.Button();
+            this.lblReserva = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProbarConexion
+            // 
+            this.btnProbarConexion.Location = new System.Drawing.Point(164, 352);
+            this.btnProbarConexion.Name = "btnProbarConexion";
+            this.btnProbarConexion.Size = new System.Drawing.Size(75, 23);
+            this.btnProbarConexion.TabIndex = 13;
+            this.btnProbarConexion.Text = "Probar COnexion";
+            this.btnProbarConexion.UseVisualStyleBackColor = true;
+            // 
+            // lblReserva
+            // 
+            this.lblReserva.AutoSize = true;
+            this.lblReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblReserva.Location = new System.Drawing.Point(145, 37);
+            this.lblReserva.Name = "lblReserva";
+            this.lblReserva.Size = new System.Drawing.Size(390, 46);
+            this.lblReserva.TabIndex = 12;
+            this.lblReserva.Text = "Mantenedor Reserva";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(569, 391);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(135, 23);
+            this.btnVolver.TabIndex = 11;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(569, 263);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(135, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Cambiar Estado";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(413, 263);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(135, 23);
+            this.btnListar.TabIndex = 9;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(248, 263);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(135, 23);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(96, 263);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(135, 23);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // MantenedorReservas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnProbarConexion);
+            this.Controls.Add(this.lblReserva);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "MantenedorReservas";
+            this.Text = "MantenedorReservas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProbarConexion;
+        private System.Windows.Forms.Label lblReserva;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
