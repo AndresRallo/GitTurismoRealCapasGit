cd03b46424ef6313f757ca4d23f05c429617286e
tercerseis <mabarcaot@gmail.com>
2022-06-11 12:02:44 -0400

    Push pre clases :v

--- CapaPresentacion/ReservaDepto/VerReservadepto.cs
+++ CapaPresentacion/ReservaDepto/VerReservadepto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,22 @@ namespace CapaPresentacion.Reserva
 {
     public partial class VerReservadepto : Form
     {
+        
+
         public VerReservadepto()
         {
             InitializeComponent();
+            CNReservaDpto cNReserva = new CNReservaDpto();
+            dataGridView1.DataSource = cNReserva.ListALLReserva();
+
         }
 
         private void VerReserva_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
